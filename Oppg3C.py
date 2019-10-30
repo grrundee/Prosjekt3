0dac2f3111bff7990fdeab816565fbb59840732f
Grunde <grunde27@gmail.com>
2019-10-30 13:01:27 +0100

    Fullført Oppg3C.py

---
+++ Oppg3C.py
@@ -0,0 +1,23 @@
+# Navn: Grunde Gregersen
+# Studentnummer: 220511
+
+# Imprterer library
+import numpy as np 
+
+def f(x):
+    return((x**2)*np.sin(x-2)-4*x) # x E [15/2,10]
+
+def dfdx(x):
+    return (x * (x * (np.sin(2) * np.sin(x) + np.cos(2) * np.cos(x)) - 2 * np.sin(2 - x)) - 4)
+
+x = 15/2
+stopval = 10**(-10)
+i = 0
+
+while (abs(f(x))>stopval and i<100):
+    x-=(f(x)/dfdx(x))
+    i+=1
+    print("Tilnermet løsning for x =",x)
+    print("Dette er irretasjon nr.", i)
+
+Print("Vi ser at metoden jobber seg inn mot et nullpunkt og vi finner ett annet nullpunkt enn i oppgave B")
